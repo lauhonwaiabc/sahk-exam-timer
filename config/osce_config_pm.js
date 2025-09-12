38ca310d09defdaeaf417d2d39eb15057947042b
lauhonwaiabc <109023091+lauhonwaiabc@users.noreply.github.com>
2025-09-12 11:50:50 +0800

    Delete config directory

--- config/osce_config_pm.js
+++
@@ -1,8 +0,0 @@
-const START_TIME = "13:30:00";
-const NUM_SESSIONS = 15;
-const NUM_STATIONS = 15;
-
-const standardPhases = [
-    {title: 'Transit', duration: 120, info: '2 minutes'},
-    {title: 'OSCE Exam', duration: 600, info: '10 minutes'}
-];
