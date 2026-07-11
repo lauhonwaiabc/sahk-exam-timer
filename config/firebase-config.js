745f4e08e41769d7b07a1d15046442370bbdf623
lauhonwaiabc <109023091+lauhonwaiabc@users.noreply.github.com>
2026-07-11 19:46:06 +0800

    Delete config directory

--- config/firebase-config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Sahk.register('FirebaseConfig', function() {
-  return {
-    apiKey: 'AIzaSyAJQzjpcfrGkpRRkTmayseHeh8nxRDds2s',
-    authDomain: 'sahk-timer.firebaseapp.com',
-    projectId: 'sahk-timer',
-    appId: '1:1053340816109:web:30d82cca029e481a0bb2e4'
-  };
-});
