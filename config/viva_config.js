2400a8d447177c40fdf7b191a8d5b1edcc53ad80
lauhonwaiabc <109023091+lauhonwaiabc@users.noreply.github.com>
2026-08-16 13:01:06 +0800

    Refactor viva_config.js for clarity and structure

--- config/viva_config.js
+++ config/viva_config.js
@@ -1,7 +1,7 @@
 'use strict';
 const START_TIME_AM = "08:30:00";
 const START_TIME_PM = "13:25:00";
-const NUM_STATIONS = 10;
+const NUM_TABLES = 10;
 const BASE_NUM_SESSIONS = 10;
 
 const ENABLE_PREPARATION = true;
