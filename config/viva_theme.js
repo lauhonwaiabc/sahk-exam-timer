949fe0bff766b2410add8ce299739bd6ef75a548
lauhonwaiabc <109023091+lauhonwaiabc@users.noreply.github.com>
2026-09-09 17:39:17 +0800

    Update viva_theme.js

--- config/viva_theme.js
+++ config/viva_theme.js
@@ -2,25 +2,25 @@
 const VIVA_THEMES_AM = [
   "theme_1_am",
   "theme_2_am",
-  "theme_3_am",
-  "theme_4_am",
+  "COPD, prehabilitation, esophagectomy",
+  "Duchenne muscular dystrophy, appendicectomy",
   "theme_5_am",
-  "theme_6_am",
-  "theme_7_am",
-  "theme_8_am",
+  "Retrosternal goiter, ischemic CMP with CRT-D",
+  "Athlete, shoulder surgery, RA; SLE, antiphospholipid syndrome, cancer surgery",
+  "Thyroid bleed ",
   "Paediatric airway",
   "theme_10_am"
 ];
 
 const VIVA_THEMES_PM = [
   "theme_1_pm",
-  "theme_2_pm",
+  "Pre-eclampsia Spectrum Disorders",
   "theme_3_pm",
-  "theme_4_pm",
+  "Duchenne muscular dystrophy, appendicectomy",
   "theme_5_pm",
-  "theme_6_pm",
+  "Retrosternal goiter, ischemic CMP with CRT-D",
   "theme_7_pm",
-  "theme_8_pm",
-  "theme_9_pm",
+  "Acute airway in patient with ultra high cardiovascular risk",
+  "DM and SGLT2/GLP1 RA, gastric USG, mediastinoscopy",
   "theme_10_pm"
 ];
